189c708a4e8ab18d9c3a7164b17ca4e25f877830
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-09-06 12:30:12 -0300

    ensure LocalSavePurchases insert cache with correct key

--- src/data/usercases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usercases/save-purchases/local-save-purchases.spec.ts
@@ -2,14 +2,20 @@ import { CacheStore } from "@/data/protocols/cache";
 import { LocalSavePurchases } from "@/data/usercases";
 
 class CacheStoreSpy implements CacheStore {
-  deleteCallCount = 0;
+  deleteCallsCount = 0;
   insertCallsCount = 0;
-  key: string
+  deleteKey: string
+  insertKey: string
 
   delete(key: string): void {
-    this.deleteCallCount++
-    this.key = key
+    this.deleteCallsCount++
+    this.deleteKey = key
   };
+
+  insert(key: string): void {
+    this.insertCallsCount++
+    this.insertKey = key
+  }
 };
 
 type SutType = {
@@ -31,22 +37,30 @@ describe('LocalSavePurchases', () =>{
   test('should not delete cache on sut.init', () => {
     const { cacheStore } = makeSut();
     new LocalSavePurchases(cacheStore);
-    expect(cacheStore.deleteCallCount).toBe(0);
+    expect(cacheStore.deleteCallsCount).toBe(0);
   });
 
   test('should delete old cache save on sut.save', async () => {
     const { sut, cacheStore } = makeSut();
     await sut.save();
 
-    expect(cacheStore.deleteCallCount).toBe(1);
-    expect(cacheStore.key).toBe('purchases');
+    expect(cacheStore.deleteCallsCount).toBe(1);
+    expect(cacheStore.deleteKey).toBe('purchases');
   });
 
-  test('should not insert new cache if delete fails',  () => {
+  test('should not insert new cache if delete fails', () => {
     const { cacheStore, sut } = makeSut();
     jest.spyOn(cacheStore, 'delete').mockImplementationOnce(() => { throw new Error() });
     const promise = sut.save();
     expect(cacheStore.insertCallsCount).toBe(0);
     expect(promise).rejects.toThrow();
+  });
+
+  test('should insert new cache if delete succeeds',  async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.insertCallsCount).toBe(1);
+    expect(cacheStore.deleteCallsCount).toBe(1);
+    expect(cacheStore.insertKey).toBe('purchases');
   });
 });
