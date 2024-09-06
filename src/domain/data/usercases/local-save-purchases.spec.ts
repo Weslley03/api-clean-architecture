41b929a0121a5ed422cd7054ca534e8fcbb3b528
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-09-06 10:33:01 -0300

    ensure LocalSavePurchases does not delete cache upon creation

--- src/domain/data/usercases/local-save-purchases.spec.ts
+++ src/domain/data/usercases/local-save-purchases.spec.ts
@@ -1,5 +1,19 @@
-describe('local savepurchases', () =>{
-  test('', () => {
-    expect(1).toBe(1);
+class LocalSavePurchases{
+  constructor(private readonly cacheStore: CacheStore) {}
+};
+
+interface CacheStore {
+  //
+}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallCount = 0
+}
+
+describe('LocalSavePurchases', () =>{
+  test('should not delete cache on sut.init', () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallCount).toBe(0);
   });
 });
