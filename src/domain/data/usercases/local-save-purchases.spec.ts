4ed48f8db0b936ccf8ef2f59f1e30cc1a0a85083
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-09-06 10:40:30 -0300

    ensure LocalSavePurchases deletes old cache on save

--- src/domain/data/usercases/local-save-purchases.spec.ts
+++ src/domain/data/usercases/local-save-purchases.spec.ts
@@ -1,13 +1,22 @@
 class LocalSavePurchases{
   constructor(private readonly cacheStore: CacheStore) {}
+  
+  async save(): Promise<void> {
+    this.cacheStore.delete()
+  }
 };
 
 interface CacheStore {
-  //
+  delete: () => void
+
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallCount = 0
+
+  delete(): void {
+    this.deleteCallCount++
+  }
 }
 
 describe('LocalSavePurchases', () =>{
@@ -15,5 +24,13 @@ describe('LocalSavePurchases', () =>{
     const cacheStore = new CacheStoreSpy();
     new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallCount).toBe(0);
+  });
+
+  test('should delete old cache save on sut.save', async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+
+    expect(cacheStore.deleteCallCount).toBe(1);
   });
 });
