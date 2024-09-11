dd280887664002f54c3937860d3916fd4ede7f75
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-09-11 03:45:19 -0300

    ensure LocalLoadPurchases returns an empty list if cache is more than 3 days old

--- src/data/usercases/load-purchases/local-load-purchases.ts
+++ src/data/usercases/load-purchases/local-load-purchases.ts
@@ -18,7 +18,14 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
   async loadAll(): Promise<Array<LoadPurchases.Result>> {
     try{
       const cache = this.cacheStore.fetch('purchases');
-      return cache.value
+      const maxAge = new Date(cache.timestamp);
+      maxAge.setDate(maxAge.getDate() +3);
+
+      if(maxAge > this.currentDate) {
+        return cache.value
+      } else {
+        throw new Error()
+      }
     } catch(err) {
       this.cacheStore.delete('purchases');
       return []
