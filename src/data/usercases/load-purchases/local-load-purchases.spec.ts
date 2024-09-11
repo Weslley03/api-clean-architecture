6441924e876635e8f8f3c07e950bec0aa1a49241
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-09-11 03:51:50 -0300

    ensure LocalLoadPurchases does not delete empty cache

--- src/data/usercases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usercases/load-purchases/local-load-purchases.spec.ts
@@ -86,4 +86,23 @@ describe('LocalLoadPurchases', () =>{
     expect(cacheStore.deleteKey).toBe('purchases');
     expect(purchases).toEqual([]);
   });
+
+  test('should return an empty list if cache is empty',  async () => {
+    const currentDate = new Date();
+    const timestamp = new Date(currentDate);
+    timestamp.setDate(timestamp.getDate() -3)
+    timestamp.setSeconds(timestamp.getSeconds() +1)
+
+    const { cacheStore, sut} = makeSut(currentDate);
+    
+    cacheStore.fetchResult = {
+      timestamp,
+      value: []
+    };
+
+    const purchases =  await sut.loadAll();
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
+    expect(cacheStore.fetchKey).toBe('purchases');
+    expect(purchases).toEqual([]);
+  });
 });
